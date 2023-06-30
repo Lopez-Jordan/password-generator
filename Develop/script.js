d13ebb7714c4f6371db439c8be77477e4397d719
Jordan Lopez <jordanlopezemail@gmail.com>
2023-06-29 22:23:12 -0700

    finished assignment

--- Develop/script.js
+++ Develop/script.js
@@ -83,32 +83,77 @@ function findSpecial(){
 
 
 function generatePassword(len, low, up, num, spec){
-  var pass;
+  var pass = "";
   var valsLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var valsUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var valsNumeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var valsSpecial = ["!", "@", "#", "$", "%", "&", "*", "(", ")", "-"];
+  var valsSpecial = ["!", "^", "#", "$", "%", "&", "*", "(", ")", "-"];
   var library;
   var libraryLength;
-  if (low == true && up == false && num == false && spec == false){
+  if (low == true && up == false && num == false && spec == false){ // 2
     library = valsLower;
     libraryLength = 26;
   }
-  else if (low == true && up == true && num == false && spec == false){
-    library = valsLower.concat(valsUpper);
+  else if (low == false && up == true && num == false && spec == false){ //3
+      library = valsUpper;
+      libraryLength = 26;
+  }
+  else if (low == true && up == true && num == false && spec == false){ //4
+    library = valsUpper.concat(valsLower);
     libraryLength = 52;
   }
-  else if (low == true && up == true && num == true && spec == false){
+  else if (low == false && up == false && num == true && spec == false){ //5
+    library = valsNumeric;
+    libraryLength = 9;
+  }
+  else if (low == true && up == false && num == true && spec == false){ //6
+    library = valsLower.concat(valsNumeric);
+    libraryLength = 35;
+  }
+  else if (low == false && up == true && num == true && spec == false){ //7
+    library = valsUpper.concat(valsNumeric);
+    libraryLength = 35;
+  }
+  else if (low == true && up == true && num == true && spec == false){ //8
     library = (valsLower.concat(valsUpper)).concat(valsNumeric);
     libraryLength = 61;
   }
-  else if (low == true && up == true && num == true && spec == true){
+  else if (low == false && up == false && num == false && spec == true){ //9
+    library = valsSpecial;
+    libraryLength = 10;
+  }
+  else if (low == true && up == false && num == false && spec == true){ //10
+    library = valsLower.concat(valsSpecial);
+    libraryLength = 36;
+  }
+  else if (low == false && up == true && num == false && spec == true){ //11
+    library = valsUpper.concat(valsSpecial);
+    libraryLength = 36;
+  }
+  else if (low == true && up == true && num == false && spec == true){ //12
+    library = (valsLower.concat(valsUpper)).concat(valsSpecial);
+    libraryLength = 62;
+  }
+  else if (low == false && up == false && num == true && spec == true){ //13
+    library = valsSpecial.concat(valsNumeric);
+    libraryLength = 19;
+  }
+  else if (low == true && up == false && num == true && spec == true){ //14
+    library = (valsLower.concat(valsNumeric)).concat(valsSpecial);
+    libraryLength = 10;
+  }
+  else if (low == false && up == true && num == true && spec == true){ //15
+    library = (valsUpper.concat(valsNumeric)).concat(valsSpecial);
+    libraryLength = 45;
+  }
+  else{
     library = ((valsLower.concat(valsUpper)).concat(valsNumeric)).concat(valsSpecial);
     libraryLength = 71;
   }
-  console.log(library);
-
-
+  for(var i = 0; i < len; i++){
+    var randomNumber = Math.floor(Math.random() * libraryLength);
+    pass += library[randomNumber];
+  }
   return pass;
 }
 // Write password to the #password input
@@ -129,6 +174,7 @@ function writePassword() {
   }
 
   var password = generatePassword(length, lower, upper, numeric, special);
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
