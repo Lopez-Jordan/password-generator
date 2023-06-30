2a5843b16b5d5d0e5bd712cfee6e43de7ddf8083
Jordan Lopez <jordanlopezemail@gmail.com>
2023-06-29 21:14:12 -0700

    finished all helper functions

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ function findLength(){
   var responseLength = prompt("What length do you want the password to be (8 to 128 characters)");
   if (isNaN(responseLength) || responseLength < 8 || responseLength > 128){
     while (isNaN(responseLength) || responseLength < 8 || responseLength > 128){
-      alert("Invalid input... You choose a number in the correct range!")
+      alert("Invalid input... You must choose a number in the correct range!")
       responseLength = prompt("What length do you want the password to be (8 to 128 characters)");
     }
   }
@@ -18,25 +18,66 @@ function findLength(){
 function findLower(){
   var wantLower = prompt("Would you like characters that are lowercase? Type 'yes' or 'no'");
   if (wantLower != "yes" || wantLower != "no"){
-    while (wantLower != "yes" || wantLower != "no"){
+    while (wantLower != "yes" && wantLower != "no"){
       alert("Invalid input...You must choose 'yes' or 'no'");
       wantLower = prompt("Would you like characters that are lowercase? Type 'yes' or 'no'");
     }
   }
-  if (wantLower == "yes"){
+  if (wantLower === 'yes'){
     return true;
   }
   else{
     return false;
   }
 }
-
 //Function for seeing if they want upper case
-
+function findUpper(){
+  var wantUpper = prompt("Would you like characters that are UPPERcase? Type 'yes' or 'no'");
+  if (wantUpper != "yes" || wantUpper != "no"){
+    while (wantUpper != "yes" && wantUpper != "no"){
+      alert("Invalid input...You must choose 'yes' or 'no'");
+      wantUpper = prompt("Would you like characters that are UPPERcase? Type 'yes' or 'no'");
+    }
+  }
+  if (wantUpper === 'yes'){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 //Function for seeing if they want numbers in the password
-
+function findNumeric(){
+  var wantNums = prompt("Would you like characters that are numeric? Type 'yes' or 'no'");
+  if (wantNums != "yes" || wantNums != "no"){
+    while (wantNums != "yes" && wantNums != "no"){
+      alert("Invalid input...You must choose 'yes' or 'no'");
+      wantNums = prompt("Would you like characters that are numeric? Type 'yes' or 'no'");
+    }
+  }
+  if (wantNums === 'yes'){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 //Function for seeing if they want special characters in the password
-
+function findSpecial(){
+  var wantSpecial = prompt("Would you like special characters? Type 'yes' or 'no'");
+  if (wantSpecial != "yes" || wantSpecial != "no"){
+    while (wantSpecial != "yes" && wantSpecial != "no"){
+      alert("Invalid input...You must choose 'yes' or 'no'");
+      wantSpecial = prompt("Would you like special characters? Type 'yes' or 'no'");
+    }
+  }
+  if (wantSpecial === 'yes'){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
